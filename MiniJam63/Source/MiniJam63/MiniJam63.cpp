262300321fe14c394cf15fadd20fa0fc4c5fd3ae
Tomas Hudak <hudakt05@hotmail.com>
2020-09-18 11:40:50 +0100

    Setup

---
+++ MiniJam63/Source/MiniJam63/MiniJam63.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MiniJam63.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MiniJam63, "MiniJam63" );
